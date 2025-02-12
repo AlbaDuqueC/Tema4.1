40442b3365b9da132bf63861d8c6476ae965dee7
AlbaDuqueC <alba.duque@iesnervion.es>
2025-02-12 11:49:36 +0100

    Libro

--- src/boletin2/ejercicio2/Libro.java
+++ src/boletin2/ejercicio2/Libro.java
@@ -2,4 +2,160 @@ package boletin2.ejercicio2;
 
 public class Libro {
 
+	private String titulo;
+	private String autor;
+	private int ejemplares;
+	private int ejemplaresPrestados;
+
+	private enum Genero {
+		Narrativo, Lírico, Dramático, Didáctico, Poético
+	}
+
+	private Genero genero = Genero.Narrativo;
+
+	public Libro(String titulo, String autor) {
+
+		if (titulo != null && !titulo.isBlank()) {
+			this.titulo = titulo;
+		}
+
+		if (autor != null && !autor.isBlank()) {
+			this.autor = autor;
+		}
+	}
+
+	public Libro(String titulo, String autor, int ejemplares, int ejemplaresPrestados) {
+
+		if (titulo != null && !titulo.isBlank()) {
+			this.titulo = titulo;
+		}
+
+		if (autor != null && !autor.isBlank()) {
+			this.autor = autor;
+		}
+
+		if (ejemplares > 0) {
+			this.ejemplares = ejemplares;
+		}
+
+		if (ejemplaresPrestados >= 0) {
+			this.ejemplaresPrestados = ejemplaresPrestados;
+		}
+	}
+
+	public Libro(String titulo, String autor, int ejemplares, int ejemplaresPrestados, String genero) {
+
+		if (titulo != null && !titulo.isBlank()) {
+			this.titulo = titulo;
+		}
+
+		if (autor != null && !autor.isBlank()) {
+			this.autor = autor;
+		}
+
+		if (ejemplares > 0) {
+			this.ejemplares = ejemplares;
+		}
+
+		if (ejemplaresPrestados >= 0) {
+			this.ejemplaresPrestados = ejemplaresPrestados;
+		}
+
+		switch (genero) {
+
+		case "Lírico" -> {
+			this.genero = Genero.Lírico;
+		}
+
+		case "Dramático" -> {
+			this.genero = Genero.Dramático;
+		}
+
+		case "Didáctico" -> {
+			this.genero = Genero.Didáctico;
+		}
+
+		case "Poético" -> {
+			this.genero = Genero.Poético;
+		}
+		}
+	}
+
+	public String getTitulo() {
+		return titulo;
+	}
+
+	public String getAutor() {
+		return autor;
+	}
+
+	public void setAutor(String autor) {
+		this.autor = autor;
+	}
+
+	public int getEjemplares() {
+		return ejemplares;
+	}
+
+	public void setEjemplares(int ejemplares) {
+		this.ejemplares = ejemplares;
+	}
+
+	public int getEjemplaresPrestados() {
+		return ejemplaresPrestados;
+	}
+
+	public void setEjemplaresPrestados(int ejemplaresPrestados) {
+		this.ejemplaresPrestados = ejemplaresPrestados;
+	}
+
+	public boolean prestamo(int prestados) {
+		boolean prestar = false;
+
+		if (this.ejemplares >= prestados) {
+
+			prestar = true;
+
+			this.ejemplaresPrestados += prestados;
+			this.ejemplares -= prestados;
+		}
+
+		return prestar;
+	}
+
+	public boolean devolucion(int devolver, String titulo) {
+		boolean devolucion = false;
+
+		if (devolver <= this.ejemplaresPrestados && this.titulo == titulo) {
+			devolucion = true;
+
+			this.ejemplaresPrestados -= devolver;
+			this.ejemplares += devolver;
+		}
+
+		return devolucion;
+	}
+
+	public String toString() {
+		String info = "";
+
+		info += "Título: " + this.titulo + " Autores: " + this.autor + " Ejemplares: " + this.ejemplares
+				+ " Ejemplares prestados: " + this.ejemplaresPrestados;
+
+		return info;
+	}
+
+	public boolean equals (Object obj) {
+		
+		Libro libro2 = (Libro) obj;
+		
+		boolean igual = false;
+
+		if (titulo.equalsIgnoreCase(libro2.titulo) && autor.equalsIgnoreCase(libro2.autor)) {
+			igual = true;
+		}
+		
+		return igual;
+	}
+
 }
--- src/boletin2/ejercicio2/PrincipalLibro.java
+++ src/boletin2/ejercicio2/PrincipalLibro.java
@@ -3,7 +3,34 @@ package boletin2.ejercicio2;
 public class PrincipalLibro {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		Libro libro1;
+		Libro libro2;
+		
+		String titulo="hola";
+		String autor= "Felipe";
+		int ejemplares= 20;
+		int ejemplaresPrestados=5;
+		String genero="Lírico";
+		
+		libro1= new Libro (titulo, autor);
+		
+		libro1= new Libro (titulo, autor, ejemplares, ejemplaresPrestados);
+		
+		libro1= new Libro (titulo, autor, ejemplares, ejemplaresPrestados, genero);
+		
+		if(libro1.prestamo(5)) {
+			System.out.println("Se hizo el prestamo correctamente");
+			System.out.println("Libro prestados: "+libro1.getEjemplaresPrestados());
+			
+		}else {
+			System.out.println("No se pudo realizar el prestamo");
+		}
+		
+		if(libro1.devolucion(5, "hola")) {
+			System.out.println("Se hizo la devolucion correctamente");
+			
+		}
 
 	}
 
