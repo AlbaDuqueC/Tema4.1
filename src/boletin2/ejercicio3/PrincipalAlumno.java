31ad16aa4418e115711feac8212bebe56b670131
AlbaDuqueC <alba.duque@iesnervion.es>
2025-02-14 14:41:14 +0100

    sdfsdfsdf

--- src/boletin2/ejercicio3/PrincipalAlumno.java
+++ src/boletin2/ejercicio3/PrincipalAlumno.java
@@ -4,8 +4,15 @@ public class PrincipalAlumno {
 
 	public static void main(String[] args) {
 		
-		
+		Alumno alumno1;
+		Alumno alumno2;
 
+		String nombre="Pepe";
+		double notaMedia=5.4;
+		
+		
+		
+		
 	}
 
 }
