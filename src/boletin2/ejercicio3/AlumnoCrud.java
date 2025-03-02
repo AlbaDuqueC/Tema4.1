685134a25637e71d5848c6972c89e676b311ec9d
AlbaDuqueC <alba.duque@iesnervion.es>
2025-03-02 18:05:26 +0100

    boletin 4 -> 1

--- src/boletin2/ejercicio3/AlumnoCrud.java
+++ src/boletin2/ejercicio3/AlumnoCrud.java
@@ -10,7 +10,45 @@ public class AlumnoCrud {
 		for (Alumno a : Alumnos) {
             System.out.println(a);
             System.out.println("-------------------------------");
+           
         }
+	}
+	
+	static public boolean NuevoAlumno (Alumno nombre) {
+		return  Alumnos.add(nombre);
+		
+	}
+	
+	static public Alumno Buscar (String nombre) {
+		Alumno a = null; 
+
+		for (Alumno aBusqueda : Alumnos) {
+			if (aBusqueda.getNombre().equals(nombre)) {
+				a = aBusqueda;
+			}
+		}
+
+		return a;
+
+	}
+	public static boolean Borrar(Alumno a) {
+		return Alumnos.remove(a);
+	}
+	
+	static public boolean Modificar (String nombre, double media) {
+		
+		boolean si=  false;
+		
+		Alumno a = Buscar(nombre);
+		if (a != null) {
+			a.setNotaMedia(media);
+			si=true;
+		}
+		
+		return si; 
+		
+		
+		
 	}
 	
 	
--- src/boletin2/ejercicio3/PrincipalAlumno.java
+++ src/boletin2/ejercicio3/PrincipalAlumno.java
@@ -1,19 +1,105 @@
 package boletin2.ejercicio3;
 
+import java.util.Scanner;
+
 public class PrincipalAlumno {
+	static Scanner sc = new Scanner(System.in);
 
 	public static void main(String[] args) {
-		
-		Alumno alumno1;
-		Alumno alumno2;
 
-		String nombre="Pepe";
-		double notaMedia=5.4;
 		
-		
-		
-		
-		
+
+		Alumno alumno;
+
+		String nombre;
+		double notaMedia;
+
+		int opcion;
+
+		do {
+
+			System.out.println("-------------------------------");
+			System.out.println("1. Listado.");
+			System.out.println("2. Nuevo Alumno.");
+			System.out.println("3. Modificar.");
+			System.out.println("4. Borrar.");
+			System.out.println("5. Salir.");
+
+			opcion = sc.nextInt();
+
+			switch (opcion) {
+			case 1 -> {
+
+				AlumnoCrud.Listado();
+			}
+			case 2 -> {
+				alumno = creaAlumno();
+				AlumnoCrud.NuevoAlumno(alumno);
+
+			}
+			case 3 -> {
+
+				nombre = pedirNombre();
+				System.out.println("Introduce la nueva media");
+				notaMedia = sc.nextDouble();
+
+				if (AlumnoCrud.Modificar(nombre, notaMedia)) {
+					System.out.println("Se ha podido cambiar la media");
+				} else {
+					System.out.println("No se ha podido modificar la media");
+				}
+
+			}
+
+			case 4 -> {
+
+				nombre = pedirNombre();
+				alumno = AlumnoCrud.Buscar(nombre);
+				if (alumno != null) {
+					AlumnoCrud.Borrar(alumno);
+				} else {
+					System.out.println("El alumno no existe");
+				}
+			}
+			case 5 -> System.out.println("--Saliendo del sistema--");
+
+			default -> System.out.println("Opción incorrecta");
+
+
+			}
+
+		} while (opcion != 5);
+
+	}
+
+	static Alumno creaAlumno() {
+
+		String nombre;
+
+		double media;
+
+		Alumno al;
+
+		nombre = pedirNombre();
+
+		System.out.println("Digame la media");
+		media = sc.nextDouble();
+
+		al = new Alumno(nombre, media);
+
+		return al;
+
+	}
+
+	private static String pedirNombre() {
+
+		String nombre;
+
+		System.out.println("Digame el nombre del alumno");
+		nombre = sc.nextLine();
+
+		return nombre;
+
 	}
 
 }
